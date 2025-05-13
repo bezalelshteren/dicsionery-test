5a770f723140972908becd83f4aad37efe5d4ec2
bezalelshteren <a0556722346@gmail.com>
2025-05-13 13:07:51 +0300

    in the midel to fix

--- source/repos/numOperasionProjekt/numOperasionProjekt/Program.cs
+++ source/repos/numOperasionProjekt/numOperasionProjekt/Program.cs
@@ -10,23 +10,44 @@ namespace numOperasionProjekt
     {
 
     
-        static int[] getThenumber(int[] args)
+        static List<int> getThenumber(int[] args)
         {
-            if Not(args)
+            if (args.Length == 0)
             {
-                getNumberBycw()
+                getNumberBycw();
             }
+            return args;
+        }
+    
+
+
+
+        //ממיר לרשימה של מספרים
+        
+        static List<int> chaingToNumList()
+        {
+            
+            List<string> numbersList = new List<string>(getNumberBycw().Split(' '));
+            List<int> isnumbers = new List<int>();
+            foreach (string number in numbersList)
+            {
+                isnumbers.Add(int.Parse(number));
+            }
+            return isnumbers;
         }
 
 
-
         //a
-        static int getNumberBycw()
+        static string getNumberBycw()
         {
-            do {
-            Console.WriteLine("enter numbers");
-            int[] numbersList = Convert.Int32 (Console.ReadLine().Split(' '));
-            while(numbersList.Length > 3 )
+            string numbersList;
+            do
+            {
+                Console.WriteLine("enter numbers");
+                numbersList = Console.ReadLine();
+            }
+            while (numbersList.Length > 3);
+            
             return numbersList;
         }
 
@@ -35,94 +56,161 @@ namespace numOperasionProjekt
 
         static void printOrder(int[] number)
         {
-            foreach(int number in getThenumber(0))
-                    Console.WriteLine(number);
+            foreach (int num in number) {
+                Console.WriteLine(num);
+            }
         }
 
 
 
-            //c
-            static void printRevers(int[] number = [5,6,9,73,14,74])
+        //c
+        static void printRevers(int[] number)
+        {
+            int pointer = number.Length;
+            while (pointer > 0) {
+                Console.WriteLine(number[pointer]);
+                pointer--;
+            }
+        }
+
+
+
+
+
+        //d 
+
+        static void printSorted(List<int> number)
+        {
+            List<int> numberTosorted = number;
+                        for (int i = 0; i < numberTosorted.Count; i++)
             {
-                int pointer = number.Length;
-                while(pointer > 0) {
-                    Console.WriteLine(number[pointer]);
-                    pointer--;
+                for (j = 0; j < numberTosorted.Count - 1; j++)
+                {
+                    if (numberTosorted[j] > numberTosorted[j + 1])
+                    {
+                        numberTosorted[numberTosorted.Count - 1] = numberTosorted[j];
+                                    j++;
+                            }
+                }
+                Console.WriteLine(numberTosorted);
+              }
+
+
+
+            //e
+            static void printMax(List<int> number2)
+            {
+                int theMex = 0;
+                foreach (int num in number2)
+                {
+                    if (num > theMex)
+                    {
+                        theMex = num;
+                    }
+                }
+                Console.WriteLine(theMex);
+            }            
+
+            //f
+
+            static void printMin(List<int> number2) {
+
+                int theMin = 0;
+                foreach (int num in number2)
+                {
+                    if (num < theMin)
+                    {
+                        theMin = num;
+                    }
+                }
+                Console.WriteLine(theMin);
+            }
+
+            //g
+
+            static void printAverage(List<int> number3, int sum)
+            {
+                int len = number3.Count;
+                int average = sum / len;
+                Console.WriteLine(average);
+            }
+
+        //h
+
+            static void printLen(int[] number)
+            {
+
             }
 
 
+        //i
 
 
-
-                //d 
-
-                static int[] printSorted(int[] number)
+        static int printSum(int[] number)
+            {
+                int sum = 0;
+                for (int i = 0; i < number.Length; i++)
                 {
-                    int [] numberTosorted =  number
-                        for (int i = 0;i<numberTosorted.Length;i++) 
-                             {
-                        for ( j = 0; j < numberTosorted.Length-1; j++)
-                        {
-                            if (numberTosorted[j] > numberTosorted[j + 1])
-                            {
-                                numberTosorted[numberTosorted.Length -1] = numberTosorted[j]
-                                    j++
-                            }
-                        }
-                        Console.WriteLine(numberTosorted);
-                        int[] MaxMin = numberTosorted[0]
-                        MaxMin.add(numberTosorted[numberTosorted.Length-1]);
-                        return numberTosorted;
-                    }
+                    sum += number[i];
+                }
+                Console.WriteLine(sum);
+                return sum;
+            }
+            
 
 
 
-                    //e
-                    static void printMax(int[] numberToSorted) {
-                        Console.WriteLine(numberTosorted[numberTosorted.Length - 1])
-                            }
 
-                    //f
-
-                    static void printMin(int[] numberToSorted) {
-                        Console.WriteLine(numberTosorted[0])
-                            }
-
-                    //g
-
-                    static int printSum(int[] number)
-                    {
-                        int sum = 0;
-                        for (int i = 0; i < point; i++)
-                        {
-                            sum += number[i];
-                        }
-                        
-                        return sum;
-                    }
-
-                    //h
-
-
-
-                    //i
-
-                    
-                    //j
-
-                    static void printSum(int sum)
-                    {
-                        int salution = sum / point;
-                        Console.WriteLine(salution);
-                    }
+            static int deccide()
+            {
+                console.WriteLine("enter your choise between 1-8 !!")
+                    int choise = Convert.ToInt32(Console.ReadLine())
+                    return choise;
+            }
                     //main
 
-                    static void 
+                    static void maneger()
+                    {
+                chaingToNumList();
+                
+                deccide();
+                do
+                {
+                    switch (decisen)
+                    {
+                        case 1:
+                            printOrder();
+                            break;
+                        case 2:
+                            printRevers();
+                            break;
+                        case 3:
+                            printSorted();
+                            break;
+                        case 4:
+                            printMax();
+                            break;
+                        case 5:
+                            printMin();
+                            break;
+                        case 6:
+                            printAverage();
+                            break;
+                        case 7:
+                            printLen();
+                            break;
+                        case 8:
+                            printSum();
+                            break;
+
+
+                    }
+                    while (deccide != 8)}
 
 
                     static void Main(string[] args)
         {
-                   
+                   maneger();
         }
     }
 }
